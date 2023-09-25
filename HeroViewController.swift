a74b160d1fe530f46a67fe02ecde5d10ec4b6f26
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-09-25 15:31:09 -0300

    Initial commit

---
+++ HeroViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HeroViewController.swift
+//  HeroisMarvel
+//
+//  Created by Eric Brito on 22/10/17.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class HeroViewController: UIViewController {
+
+   
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    var hero: Hero!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let x = hero.urls.first{ $0.type == "wiki"}
+        guard let n = x else { return }
+        
+        let url = URL(string: n.url)
+        let request = URLRequest(url: url!)
+        title = hero.name
+
+        webView.allowsBackForwardNavigationGestures = true
+        webView.navigationDelegate = self
+        webView.load(request)
+        
+    }
+
+   
+}
+
+extension HeroViewController: WKNavigationDelegate{
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loading.stopAnimating()
+    }
+}
